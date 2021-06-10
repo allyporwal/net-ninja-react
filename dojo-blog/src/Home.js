4f2e70ca6ef5f40ce1ca85d04c6805d938e68e7e
allyporwal <allyporwal@gmail.com>
2021-06-10 20:11:04 +0000

    Remove app.css and test onClick functions

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    
+    const handleClick = () => {
+        console.log("Console log");
+    }
+
+    const noClickMe = (name) => {
+        console.log("Console log " + name);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={noClickMe}>No, click me</button>
         </div>
     );
 }
