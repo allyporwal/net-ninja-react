61ddab3b906600c03796ac2872da151e7bf9aacc
allyporwal <allyporwal@gmail.com>
2021-06-21 14:02:59 +0000

    Add map function to home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,18 +1,21 @@
-const Home = () => {
-    
-    const handleClick = () => {
-        console.log("Console log");
-    }
+import { useState } from 'react';
 
-    const noClickMe = (name) => {
-        console.log("Console log " + name);
-    }
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
+
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={noClickMe}>No, click me</button>
+           {blogs.map((blog) => (
+               <div className="blog-preview" key={blog.id}>
+                   <h2>{ blog.title }</h2>
+                   <p>Written by {blog.author}</p>
+               </div>
+           ))}
         </div>
     );
 }
