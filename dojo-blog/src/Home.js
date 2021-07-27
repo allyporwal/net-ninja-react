fbddcc90eefe9b8bf10fa34ba7fd45476424751b
allyporwal <allyporwal@gmail.com>
2021-07-27 18:24:07 +0000

    Add content to lesson 24

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,20 +1,16 @@
-import { useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+  const { data: blogs, isPending, error } = useFetch('https://8000-violet-coral-jquvvvki.ws-eu11.gitpod.io/blogs')
 
-
-    return (
-        <div className="home">
-           <BlogList blogs={blogs} title={"All Blogs"}/>
-           <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"Mario's Blogs"}/>
-        </div>
-    );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
+      { isPending && <div>Loading...</div> }
+    </div>
+  );
 }
 
 export default Home;
